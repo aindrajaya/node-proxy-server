1e413d00455731f5d66663afc17c1e50be028d1b
aindrajaya <arista.indrajaya@gmail.com>
2026-03-25 21:45:51 +0700

    fix: set instances to 2 for tmat-auth-proxy in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'tmat-auth-proxy',
     script: './dist/index.js',
-    instances: 'max',          // 1 worker per CPU core
+    instances: '2',          // 1 worker per CPU core
     exec_mode: 'cluster',
     watch: false,
     env_production: {
